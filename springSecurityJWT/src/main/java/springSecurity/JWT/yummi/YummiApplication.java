f5783a98e1d497a42eac03d70074e388b9c4808c
gunha <wh8299@gmail.com>
2025-01-18 14:28:24 +0900

    feat : springSecurityJWT init

---
+++ springSecurityJWT/src/main/java/springSecurity/JWT/yummi/YummiApplication.java
@@ -0,0 +1,13 @@
+package springSecurity.JWT.yummi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YummiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YummiApplication.class, args);
+	}
+
+}
---
+++ springSecurityJWT/src/main/java/springSecurity/JWT/yummi/controller/AdminController.java
@@ -0,0 +1,4 @@
+package springSecurity.JWT.yummi.controller;
+
+public class AdminController {
+}
